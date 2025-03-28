bafd1b8403c0b03af2042fa2a994c029de75eca1
desko-petkova <desko.petkova@gmail.com>
2025-03-28 09:40:02 +0200

    adding excellent project

---
+++ IfOperator/Excellent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excellent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter grade:");
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not Excellent!");
+            }
+        }
+    }
+}
