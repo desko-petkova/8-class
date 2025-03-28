b588195a67d6852518398291c72c0ea0d308394c
desko-petkova <desko.petkova@gmail.com>
2025-03-28 09:50:33 +0200

    more tasks added

---
+++ IfOperator/BiggerNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter B=");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("The bigger number is " + a);
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("The numbers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The bigger number is " + b);
+            }
+        }
+    }
+}
---
+++ IfOperator/guessPassword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre username:");
+            string user = Console.ReadLine();
+
+            Console.WriteLine("Entre possword:");
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome " + user + "!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password");
+            }
+        }
+    }
+}
